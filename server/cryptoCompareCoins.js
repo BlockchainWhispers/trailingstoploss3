24dee39ef2bf9dfd50edd4a8defdfbf0e89c08ba
constantinescusebastian <cosmic_ascension@yahoo.com>
2018-05-07 01:30:32 +0300

    remove BCH fix

--- server/cryptoCompareCoins.js
+++ server/cryptoCompareCoins.js
@@ -23,12 +23,12 @@ function addLogo(coins, balances){
             balances[coinName].tradingPairs = [];
             //console.log(balances[coinName]);
         }
-        if(coinName == "BCC"){
-            balances['BCC'].logoUrl = "https://www.cryptocompare.com" + coins['BCH'].ImageUrl;
-            balances['BCC'].coinName = coins['BCH'].CoinName;
-            balances['BCC'].symbol = coins['BCH'].Symbol;
-            balances['BCC'].tradingPairs = [];         
-        }
+        // if(coinName == "BCC"){
+        //     balances['BCC'].logoUrl = "https://www.cryptocompare.com" + coins['BCH'].ImageUrl;
+        //     balances['BCC'].coinName = coins['BCH'].CoinName;
+        //     balances['BCC'].symbol = coins['BCH'].Symbol;
+        //     balances['BCC'].tradingPairs = [];         
+        // }
     }
   }
 };

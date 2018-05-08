e5a31bc232eaef3ad2b7a4bca3844dd8f38a68a9
alex <alex@applied-risk.com>
2018-05-08 15:26:54 +0200

    Added Basic Windows Installer and Starter
    Basic windows installer and starter. The installer works correct if node is installed. The start script is shitty, will be updated soon.

---
+++ startWindows.bat
@@ -0,0 +1,3 @@
+ECHO Starting server for StopLossTrailing>CON
+ECHO The server can be found at http://localhost:3000 .Point your browser at this url and enter your API keys from BINANCE.>CON
+node app.js

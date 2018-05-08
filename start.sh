3ec214e88aa6a4502ee00dea2d69fe2980ccf17c
alex <alex@applied-risk.com>
2018-05-08 12:09:32 +0200

    Start Script Update
    Updated start script to have exit handlers. When the user wants to quit the process, it exists cleanly and makes sure no PID is left behind to block future starts of the same server.

--- start.sh
+++ start.sh
@@ -4,10 +4,24 @@ clear
 stty erase '^?'
 echo "Starting Server. Stop the server using Ctrl+c. The server will be running as long as the"
 echo "computer is up. Beware of sleep."
-echo "Server can be found at http://localhost:3000"
-if node app.js
+node app.js &
+export PID=$!
+
+if $pid
 then
-	echo "Started Server"
+	echo "Started Server at http://localhost:3000. Point your browser to this address"
 else 
 	echo "Could not start server"
+	exit 2
 fi
+
+sigquit()
+{
+   echo "signal QUIT received"
+   kill -9 $pid	   	
+}
+
+while [ 1 ]
+do
+    sleep 2
+done

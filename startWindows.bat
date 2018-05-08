572b9b2a1a34623e2ee981a0ee52bf96d86fb609
alex <alex@applied-risk.com>
2018-05-08 15:47:54 +0200

    update for windows
    Update for windows start script.

--- startWindows.bat
+++ startWindows.bat
@@ -1,3 +1,4 @@
+@echo off
 ECHO Starting server for StopLossTrailing>CON
 ECHO The server can be found at http://localhost:3000 .Point your browser at this url and enter your API keys from BINANCE.>CON
 node app.js
